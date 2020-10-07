dcd265212465d8ea25f8ac27e1643229cff8cf79
Dom Waterson <dom_waterson@hotmail.com>
2020-10-07 20:20:45 +0100

    testing firebase connection

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import React from "react";
-import "./App.css";
+import React, { useEffect, useState, useContext } from "react";
+
+import { FirebaseContext } from "./context";
 
 function App() {
+  const { firebase } = useContext(FirebaseContext);
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    firebase
+      .firestore()
+      .collection("notes")
+      .onSnapshot((serverUpdate) => {
+        const notes = serverUpdate.docs.map((_doc) => {
+          const data = _doc.data();
+          data["id"] = _doc.id;
+          return data;
+        });
+        setNotes(notes);
+      });
+  }, [firebase]);
+
   return (
     <div>
       <h1>Evernote Clone!</h1>
+      <ol>
+        {notes.map((note) => (
+          <li key={note.id}>{note.title}</li>
+        ))}
+      </ol>
     </div>
   );
 }
