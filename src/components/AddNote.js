6575c45453da552bb604892bcbb22355e7af1223
Dom Waterson <dom_waterson@hotmail.com>
2021-03-08 16:35:09 +0000

    Sidebar mutations (#8)
    * updating mutation now updates the sidebar correctly
    
    * updating mutation now updates the sidebar correctly
    
    * updating selected note on mutation success
    
    * added react query dev tools, tweaked mutation onsuccess logics
    
    * added useCallback instead of useDebounce custom hook

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -1,21 +1,28 @@
 import { useState } from "react";
 import { useMutation, useQueryClient } from "react-query";
-import { Button } from "@material-ui/core";
+import { Button, Input } from "@material-ui/core";
 
 import { useAuth } from "@/lib/auth";
 import { createNote } from "@/lib/db";
+import { useSelectedNote } from "@/context/selectedNote";
 
-function AddNote() {
+function AddNote({ setAddingNote }) {
   const { user } = useAuth();
   const queryClient = useQueryClient();
+  const { setSelectedNote } = useSelectedNote();
 
   const [title, setTitle] = useState();
 
   const newNoteMutation = useMutation((note) => createNote(note), {
     onSuccess: (data) => {
-      queryClient.setQueryData(["notes", user.token], (oldData) => ({
-        notes: [data, ...oldData.notes],
-      }));
+      queryClient.setQueryData(["notes", user.token], (oldData) => {
+        setTitle(null);
+        setAddingNote(false);
+        setSelectedNote(data);
+        return {
+          notes: [data, ...oldData.notes],
+        };
+      });
     },
   });
 
@@ -23,21 +30,20 @@ function AddNote() {
     const note = {
       authorId: user.uid,
       title: title,
-      body: "",
+      body: "<p><br><p>",
     };
 
     newNoteMutation.mutate(note);
-
-    setTitle(null);
   };
 
   return (
     <div>
-      <input
+      <Input
+        fullWidth
         type="text"
         placeholder="Enter note title"
         onKeyUp={(e) => setTitle(e.target.value)}
-      ></input>
+      />
       <Button onClick={newNoteUpdate}>Submit Note</Button>
     </div>
   );
--- src/components/NoteEditor.js
+++ src/components/NoteEditor.js
@@ -1,15 +1,16 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { useMutation, useQueryClient } from "react-query";
 import dynamic from "next/dynamic";
 const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
 import "react-quill/dist/quill.snow.css";
 import { withStyles } from "@material-ui/core/styles";
+import Input from "@material-ui/core/Input";
 
 import styles from "@/styles/noteEditor";
-import { useDebounce } from "@/hooks/useDebounce";
 import { updateNote } from "@/lib/db";
 import { useAuth } from "@/lib/auth";
 import { useSelectedNote } from "@/context/selectedNote";
+import { debounce } from "@/utils/helpers";
 
 function Editor({ classes }) {
   const { selectedNote } = useSelectedNote();
@@ -17,19 +18,33 @@ function Editor({ classes }) {
   const [body, setBody] = useState(selectedNote.body);
   const [id, setId] = useState(selectedNote.id);
 
-  const titleDebounce = useDebounce(title, 1500);
-  const bodyDebounce = useDebounce(body, 1500);
+  const updateDB = useCallback(
+    debounce(
+      (title, body, id) =>
+        updateNoteMutation.mutate({
+          title,
+          body,
+          id,
+        }),
+      1500
+    ),
+    []
+  );
 
   const queryClient = useQueryClient();
   const { user } = useAuth();
 
   const updateNoteMutation = useMutation((note) => updateNote(note), {
     onSuccess: (data) => {
-      queryClient.setQueryData(["notes", user.token], (oldData) => ({
-        notes: oldData.notes.map((cachedNote) =>
-          cachedNote.id === data.id ? { ...cachedNote, ...data } : cachedNote
-        ),
-      }));
+      queryClient.setQueryData(["notes", user.token], (oldData) => {
+        const updatedNotes = oldData.notes.filter(
+          (cachedNote) => cachedNote.id !== data.id
+        );
+
+        updatedNotes.unshift(data);
+
+        return { notes: updatedNotes };
+      });
     },
   });
 
@@ -40,21 +55,19 @@ function Editor({ classes }) {
   }, [selectedNote]);
 
   useEffect(() => {
-    updateNoteMutation.mutate({
-      title,
-      body,
-      id,
-    });
-  }, [titleDebounce, bodyDebounce]);
+    if (title !== selectedNote.title || body !== selectedNote.body) {
+      updateDB(title, body, id);
+    }
+  }, [title, body]);
 
   return (
     <div className={classes.editorContainer}>
-      <input
+      <Input
         className={classes.titleInput}
         placeholder="Note title..."
         value={title}
         onChange={(e) => setTitle(e.target.value)}
-      ></input>
+      />
       <ReactQuill value={body} onChange={(body) => setBody(body)} />
     </div>
   );
